d03c8d937caaeeee018f8a283574621c0cc7272e
tholakelebusi <tholakelebusi@gmail.com>
2020-10-14 23:35:41 +0100

    2 files modified

--- src/employees/employees.component.ts
+++ src/employees/employees.component.ts
@@ -1,54 +1,53 @@
-import { Component, OnInit } from '@angular/core';
-import { DialogComponent } from '../dialog/dialog.component';
-import {MatDialog} from '@angular/material/dialog';
-import { RecordService } from '../record.service';
+import { Component, OnInit } from "@angular/core";
+import { DialogComponent } from "../dialog/dialog.component";
+import { MatDialog } from "@angular/material/dialog";
+import { RecordService } from "../record.service";
 
 @Component({
-  selector: 'app-employees',
-  templateUrl: './employees.component.html',
-  styleUrls: ['./employees.component.css']
+  selector: "app-employees",
+  templateUrl: "./employees.component.html",
+  styleUrls: ["./employees.component.css"]
 })
 export class EmployeesComponent implements OnInit {
+  n1: boolean = false;
+  employees = [];
+   names = '';
+  surnames = '';
+  positions = '';
+  ages = '';
+  url='';
 
-employees=[];
- 
- 
-  constructor(public dialog: MatDialog,private employ:RecordService) { }
+  constructor(public dialog: MatDialog, private employ: RecordService) {}
 
-   ngOnInit(): void {
-    this.employees= this.employ.getData();
+  ngOnInit(): void {
+    this.employees = this.employ.getData();
   }
 
-  openDialog(name){
+  openDialog(name) {
     const dialogRef = this.dialog.open(DialogComponent, {
-     
- data: "Do you want to delete?"
+      data: "Do you want to delete?"
     });
- const index=this.employees.indexOf(name);
- dialogRef.afterClosed().subscribe(result => {
-      if(result)
-       {
-       if(index>-1)
-    {
-      this.employees.splice(index,1);
-      return true;
-
-    }
+    const index = this.employees.indexOf(name);
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        if (index > -1) {
+          this.employees.splice(index, 1);
+          return true;
+        }
       }
     });
+  }
 
-}
+  update() {}
 
-update()
-{
-  
-  
-}
+  open() {
+    this.n1 = true;
+  }
 
-add()
-
-
-
-
-
+  addToLists() {
+    this.n1 = true;
+const newEmpl={image:this.url,name: this.names,surname:this.surnames,age:this.ages,position: this.positions};
+this.employees.push(newEmpl);
+     
+  }
 }
